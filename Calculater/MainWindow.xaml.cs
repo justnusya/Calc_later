819930a7d1d4b9154630141ba814a9182def4c40
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2025-04-09 21:54:32 +0300

    improved the code

--- Calculater/MainWindow.xaml.cs
+++ Calculater/MainWindow.xaml.cs
@@ -1,244 +1,286 @@
 ﻿using System;
-using System.Data;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.ComponentModel;
 
 namespace Calculater
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private bool isMenuOpen = false;
-        private Stack<string> historyStack = new Stack<string>(); // Стек для збереження історії операцій
+        private Stack<string> historyStack = new Stack<string>();
         private Stack<string> redoStack = new Stack<string>();
+        private Dictionary<string, ICommandAction> commands = new Dictionary<string, ICommandAction>();
+
         public MainWindow()
         {
             InitializeComponent();
-            foreach(UIElement el in MainRoot.Children)
+            InitializeCommands();
+
+            foreach (UIElement el in MainRoot.Children)
             {
-                if(el is Button)
+                if (el is Button button)
                 {
-                    ((Button) el).Click += Button_Click;
+                    button.Click += Button_Click;
                 }
             }
+
             menuButton.Click += MenuButton_Click;
         }
+
+        private void InitializeCommands()
+        {
+            commands["C"] = new ClearCommand();
+            commands["CE"] = new UndoCommand();
+            commands["↷"] = new RedoCommand();
+            commands["⌫"] = new BackspaceCommand();
+            commands["n²"] = new SquareCommand();
+            commands["π"] = new PiCommand();
+            commands["e"] = new ECommand();
+            commands["ln"] = new LnCommand();
+            commands["√"] = new SqrtCommand();
+            commands["="] = new EvaluateCommand();
+            commands["default"] = new AppendTextCommand();
+        }
+
         private void MenuButton_Click(object sender, RoutedEventArgs e)
         {
-            if (isMenuOpen)
-            {
-                menuColumn.Width = new GridLength(0);
-            }
-            else
-            {
-                menuColumn.Width = new GridLength(1, GridUnitType.Star);
-
-            }
-
+            menuColumn.Width = isMenuOpen ? new GridLength(0) : new GridLength(1, GridUnitType.Star);
             isMenuOpen = !isMenuOpen;
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string str = (string)((Button)e.OriginalSource).Content;
-            if (str == "00")
-            {
-                if (textlabel.Text != "" && textlabel.Text!="-" && textlabel.Text!="-0" && textlabel.Text!="00")
-                {
-                    textlabel.Text += str;
-                }
-            }
-            else if(str == "0")
-            {
-                if (textlabel.Text != "-0")
-                {
-                    textlabel.Text += str;
-                }
-            }
-            else if(str == ",")
-            {
-                if (textlabel.Text != "" && !textlabel.Text.Contains(",") && char.IsDigit(textlabel.Text[textlabel.Text.Length - 1]))
-                {
-                    textlabel.Text += str;
-                }
-            }
-            else if (char.IsDigit(str[0]) && textlabel.Text == "0")
-            {
-                textlabel.Text = str;
-            }
-            else if(str == "⌫")
-            {
-                if (textlabel.Text.Length > 0)
-                    textlabel.Text = textlabel.Text.Substring(0, textlabel.Text.Length - 1);
-            }
-            else if(str == "CE")
-            {
-                //return;
-                if (historyStack.Count > 0)
-                {
-                    redoStack.Push(textlabel.Text); // Зберігаємо поточний вираз перед скасуванням
-                    textlabel.Text = historyStack.Pop(); // Відновлюємо попередній вираз
-                }
-            }
-            else if (str == "C")
-            {
-                //textlabel.Text = "";
-                textlabel.Text = "";
-                historyStack.Clear();
-                redoStack.Clear();
-            }
-            else if (str == "n²")
-            {
-                double number;
-                if (double.TryParse(textlabel.Text, out number) && number > 0)
-                {
-                    textlabel.Text = Math.Pow(number,2).ToString();
-                }
-                else
-                {
-                    textlabel.Text = "Error";
-                }
-            }
-            else if(char.IsDigit(str[0]) && textlabel.Text == ",")
-            {
-                textlabel.Text += ",";
-            }
-            else if (str == "π")
-            {
-                double number;
-                if (textlabel.Text!="" && double.TryParse(textlabel.Text, out number))
-                {
-                    textlabel.Text = (number*Math.PI).ToString();
-                }
-                else
-                {
-                    textlabel.Text += Math.PI.ToString();
-                }
-            }
-            else if (str == "e")
-            {
-                double number;
-                if (textlabel.Text != "" && double.TryParse(textlabel.Text, out number))
-                {
-                    textlabel.Text = (number * Math.E).ToString();
-                }
-                else
-                {
-                    textlabel.Text += Math.E.ToString();
-                }
-            }
-            else if (str == "ln")
-            {
-                double number;
-                if (double.TryParse(textlabel.Text, out number) && number > 0)
-                {
-                    textlabel.Text = Math.Log(number).ToString(); 
-                }
-                else
-                {
-                    textlabel.Text = "Error";
-                }
-            }
-            else if (str == "√")
-            {
-                double number;
-                if (double.TryParse(textlabel.Text, out number) && number >= 0)
-                {
-                    textlabel.Text = Math.Sqrt(number).ToString();
-                }
-                else
-                {
-                    textlabel.Text = "Error";
-                }
-            }
-            else if(str == "+")
-            {
-                char[] operators = { '+', '-', '÷', '×' };
-                if (textlabel.Text!="" && !operators.Contains(textlabel.Text.Last()))
-                {
-                    textlabel.Text += "+";
-                }
-            }
-            else if (str == "-")
-            {
-                char[] operators = { '+', '-'};
-                if (string.IsNullOrEmpty(textlabel.Text))
-                {
-                    textlabel.Text += "-";
-                }
-                else if (!operators.Contains(textlabel.Text.Last()))
-                {
-                    textlabel.Text += "-";
-                }
-            }
-            else if (str == "×")
-            {
-                char[] operators = { '+', '-', '÷', '×' };
-                if (textlabel.Text != "" && !operators.Contains(textlabel.Text.Last()))
-                {
-                    textlabel.Text += "×";
-                }
-            }
-            else if (str == "÷")
-            {
-                char[] operators = { '+', '-', '÷', '×' };
-                if (textlabel.Text != "" && !operators.Contains(textlabel.Text.Last()))
-                {
-                    textlabel.Text += "÷";
-                }
-            }
-            else if (str == "≡")
-            {
-                return;
-            }
-            else if(str == "=")
-            {
-                try
-                {
-                    string expression = textlabel.Text.Replace("×", "*")
-                                                      .Replace(",", ".")
-                                                      .Replace("÷", "/");
-                    if (expression.Contains("/0"))
-                    {
-                        throw new Exception();
-                    }
+            string content = (string)((Button)e.OriginalSource).Content;
 
-                    string value = new DataTable().Compute(expression, null).ToString();
+            if (!commands.TryGetValue(content, out var command))
+            {
+                command = commands["default"];
+            }
 
-                    historyStack.Push(textlabel.Text); // Зберігаємо вираз перед обчисленням
-                    redoStack.Clear(); // Після нового обчислення очищаємо redoStack
+            command.Execute(this, content);
+        }
 
-                    if (value.Length > 27)
-                    {
-                        double result = Convert.ToDouble(value);
-                        textlabel.Text = result.ToString("E");
-                    }
-                    else
-                    {
-                        textlabel.Text = value;
-                    }
-                }
-                catch (Exception)
-                {
-                    textlabel.Text = "Error";
-                }
+        public void PushToHistory(string value) => historyStack.Push(value);
+        public string PopFromHistory() => historyStack.Count > 0 ? historyStack.Pop() : "";
+        public void ClearHistory() => historyStack.Clear();
+        public void PushToRedo(string value) => redoStack.Push(value);
+        public string PopFromRedo() => redoStack.Count > 0 ? redoStack.Pop() : "";
+        public void ClearRedo() => redoStack.Clear();
+        public string GetText() => textlabel.Text;
+        public void SetText(string value) => textlabel.Text = value;
+    }
+
+    public interface ICommandAction
+    {
+        void Execute(MainWindow window, string parameter);
+    }
+
+    public class ClearCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            window.SetText("");
+            window.ClearHistory();
+            window.ClearRedo();
+        }
+    }
+
+    public class UndoCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            window.PushToRedo(window.GetText());
+            window.SetText(window.PopFromHistory());
+        }
+    }
+
+    public class RedoCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            var previousState = window.PopFromRedo();
+
+            if (!string.IsNullOrEmpty(previousState))
+            {
+                window.PushToHistory(window.GetText()); 
+                window.SetText(previousState); 
+            }
+        }
+    }
+
+
+    public class BackspaceCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            var current = window.GetText();
+            if (!string.IsNullOrEmpty(current))
+            {
+                window.PushToRedo(current);
+                window.SetText(current.Substring(0, current.Length - 1));
+            }
+        }
+    }
+
+    public class SquareCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            string current = window.GetText();
+            window.PushToHistory(current);
+            if (double.TryParse(window.GetText(), out double number) && number >= 0)
+            {
+                window.PushToRedo(window.GetText());
+                window.SetText(Math.Pow(number, 2).ToString());
             }
             else
             {
-                textlabel.Text += str;
+                window.SetText("Error");
+            }
+        }
+    }
+
+    public class PiCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            string current = window.GetText();
+            window.PushToHistory(current);
+            if (double.TryParse(window.GetText(), out double number))
+                window.SetText((number * Math.PI).ToString());
+            else
+                window.SetText(window.GetText() + Math.PI.ToString());
+        }
+    }
+
+    public class ECommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            string current = window.GetText();
+            window.PushToHistory(current);
+            if (double.TryParse(window.GetText(), out double number))
+                window.SetText((number * Math.E).ToString());
+            else
+                window.SetText(window.GetText() + Math.E.ToString());
+        }
+    }
+
+    public class LnCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            string current = window.GetText();
+            window.PushToHistory(current);
+            if (double.TryParse(window.GetText(), out double number) && number > 0)
+                window.SetText(Math.Log(number).ToString());
+            else
+                window.SetText("Error");
+        }
+    }
+
+    public class SqrtCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            string current = window.GetText();
+            window.PushToHistory(current);
+            if (double.TryParse(window.GetText(), out double number) && number >= 0)
+                window.SetText(Math.Sqrt(number).ToString());
+            else
+                window.SetText("Error");
+        }
+    }
+
+    public class EvaluateCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            try
+            {
+                string expression = window.GetText()
+                    .Replace("×", "*")
+                    .Replace(",", ".")
+                    .Replace("÷", "/");
+
+                if (expression.Contains("/0"))
+                    throw new Exception();
+
+                var result = new DataTable().Compute(expression, null).ToString();
+                window.PushToHistory(window.GetText());
+                window.ClearRedo();
+
+                if (result.Length > 27)
+                {
+                    window.SetText(Convert.ToDouble(result).ToString("E"));
+                }
+                else
+                {
+                    window.SetText(result);
+                }
+            }
+            catch
+            {
+                window.SetText("Error");
+            }
+        }
+    }
+
+    public class AppendTextCommand : ICommandAction
+    {
+        public void Execute(MainWindow window, string parameter)
+        {
+            string current = window.GetText();
+
+            window.PushToHistory(current);
+
+            string[] operators = { "+", "-", "×", "÷" };
+
+            string lastNumber = current.Split(operators, StringSplitOptions.RemoveEmptyEntries).LastOrDefault() ?? "";
+
+            if (parameter == "00" && current != "" && current != "-" && !current.EndsWith("00") && current != "00")
+            {
+                if (!current.EndsWith("-0") && !current.EndsWith(" 0"))
+                    window.SetText(current + parameter);
+            }
+            else if (parameter == "0")
+            {
+                if (current == "-0")
+                    return;
+
+                if (current.StartsWith("-0") && !current.Contains(",") && current.All(c => c == '-' || c == '0'))
+                    return;
+
+                if (current != "0")
+                    window.SetText(current + parameter);
+            }
+            else if (parameter == ",")
+            {
+                if (lastNumber != "" && !lastNumber.Contains(",") && char.IsDigit(current.Last()))
+                    window.SetText(current + parameter);
+            }
+            else if ("×÷".Contains(parameter) && current != "" && !"+-×÷".Contains(current.Last()))
+            {
+                window.SetText(current + parameter);
+            }
+            else if (parameter == "+" && current != "" && !"+-×÷".Contains(current.Last()))
+            {
+                window.SetText(current + parameter);
+            }
+            else if (parameter == "-" && (current == "" || !"+-×÷".Contains(current.Last())))
+            {
+                window.SetText(current + parameter);
+            }
+            else if (char.IsDigit(parameter[0]) && current == "0")
+            {
+                window.SetText(parameter);
+            }
+            else if (char.IsDigit(parameter[0]))
+            {
+                window.SetText(current + parameter);
             }
         }
     }
